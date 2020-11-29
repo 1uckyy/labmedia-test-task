e3e39067b3169f3970e795c53cdbde179ba33dc0
Vladislav Novak <novak.vlad97@mail.ru>
2020-11-29 14:41:08 +0300

    layout complete

--- src/components/UserItem.js
+++ src/components/UserItem.js
@@ -3,10 +3,19 @@ import "./UserItem.css";
 export default function UserItem({ user }) {
   return (
     <tr>
-      <td>{user.username}</td>
+      <td className="users__table-username">{user.username}</td>
       <td>{user.email}</td>
-      <td>{user.registration_date}</td>
+      <td>
+        {new Date(user.registration_date).toLocaleString("ru-RU", {
+          year: "numeric",
+          month: "numeric",
+          day: "numeric",
+        })}
+      </td>
       <td>{user.rating}</td>
+      <td>
+        <button className="users__table-btn__delete">&times;</button>
+      </td>
     </tr>
   );
 }
--- src/components/UsersTable.js
+++ src/components/UsersTable.js
@@ -3,20 +3,23 @@ import "./UsersTable.css";
 
 export default function UsersTable(props) {
   return (
-    <table className="users__table">
-      <thead>
-        <tr>
-          <th>Имя пользователя</th>
-          <th>E-mail</th>
-          <th>Дата регистрации</th>
-          <th>Рейтинг</th>
-        </tr>
-      </thead>
-      <tbody>
-        {props.users.map((user) => (
-          <UserItem user={user} key={user.id} />
-        ))}
-      </tbody>
-    </table>
+    <div className="users__table-container">
+      <table className="users__table">
+        <thead>
+          <tr>
+            <th>Имя пользователя</th>
+            <th>E-mail</th>
+            <th>Дата регистрации</th>
+            <th>Рейтинг</th>
+            <th></th>
+          </tr>
+        </thead>
+        <tbody>
+          {props.users.map((user) => (
+            <UserItem user={user} key={user.id} />
+          ))}
+        </tbody>
+      </table>
+    </div>
   );
 }
